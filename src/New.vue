f0cf27257180200c2b882c8d6d7c19bb5418070f
Rye Crowen <ryecrowen@Admins-MacBook-Pro.local>
2017-04-07 15:59:40 -0400

    Updated New.vue page

--- src/New.vue
+++ src/New.vue
@@ -1,5 +1,7 @@
 <template>
-    <div>
+    <div class="form-group condense">
+        <h1 class="text-center">New Project Form</h1>
+
         <input
             type="text"
             class="form-control"
@@ -12,11 +14,34 @@
             placeholder="Enter project title"
             v-model="project"
         >
+
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Enter the start date"
+            v-model="startDate"
+        >
+
+        <input
+            type="text"
+            class="form-control"
+            placeholder="Enter the end date"
+            v-model="endDate"
+        >
+
         <select v-model="designer" class="form-control">
             <option disabled value="">Please select one</option>
             <option>Kevin B.</option>
-            <option>Susan S.</option>
-            <option>Ryan C.</option>
+            <option>Susan Z.</option>
+            <option>John D.</option>
+            <option>Bradford W.</option>
         </select>
     </div>
 </template>
+
+<style>
+.condense {
+    margin: 0 auto;
+    max-width: 500px;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import VueRouter from 'vue-router'
 import App from './App.vue'
 import { routes } from './routes'
 import store from './store/store'
+import VueResource from 'vue-resource'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
   mode: 'history',
