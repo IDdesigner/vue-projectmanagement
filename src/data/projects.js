84298c2e8d5549a787d236ee011a754c84dd0064
Rye Crowen <ryecrowen@Admins-MacBook-Pro.local>
2017-04-07 10:44:40 -0400

    Working on files from 2nd computer. Changed data.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,8 +1,8 @@
 import uuid from 'uuid';
 
 export default [
-    {id: uuid(), client: "Apple", project: "New kiosk unit", designer: "Kevin B.", startDate: "03/21/17", dueDate: "03/29/17"},
-    {id: uuid(), client: "Kit & Ace", project: "Complete store redo", designer: "Kevin B.", startDate: "03/21/17", dueDate: "03/29/17"},
-    {id: uuid(), client: "Space NK", project: "Geo shelves", designer: "Kevin B.", startDate: "03/21/17", dueDate: "03/29/17"},
-    {id: uuid(), client: "Microsoft", project: "Computer tables", designer: "Kevin B.", startDate: "03/21/17", dueDate: "03/29/17"}
+    {id: uuid(), client: "Apple", project: "New kiosk unit", designer: "Kevin B.", startDate: "03/21/17", dueDate: "03/25/17"},
+    {id: uuid(), client: "Kit & Ace", project: "Complete store redo", designer: "John D.", startDate: "03/11/17", dueDate: "03/15/17"},
+    {id: uuid(), client: "Space NK", project: "Geo shelves", designer: "Susan Z.", startDate: "04/01/17", dueDate: "04/03/17"},
+    {id: uuid(), client: "Microsoft", project: "Computer tables", designer: "Bradford W.", startDate: "03/17/17", dueDate: "03/29/17"}
 ];
